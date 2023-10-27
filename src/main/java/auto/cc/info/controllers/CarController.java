50d8e47dab41312d027dd57ba5018e3c129885f3
liosub <mlio64565@gmail.com>
2023-10-27 22:32:37 +0100

    add gql for list cars

--- src/main/java/auto/cc/info/controllers/CarController.java
+++ src/main/java/auto/cc/info/controllers/CarController.java
@@ -5,6 +5,8 @@ import auto.cc.info.service.CarService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -18,9 +20,8 @@ public class CarController {
     public void setCarService(CarService carService) {
         this.carService = carService;
     }
-    @RequestMapping(value = "/car_list", method= RequestMethod.GET, produces = "application/json")
-    public Page<CarCommand> listAllCars(@RequestParam(defaultValue = "0") int page
-            , @RequestParam(defaultValue = "10") int size){
+    @QueryMapping("listAllCars")
+    public Page<CarCommand> listAllCars(@Argument int page, @Argument int size){
         Page<CarCommand> carCommands = carService.listCars(page,size);
         return carCommands;
     }
