7a7105ad411f672dc5ea7f89bb396af05933c4ac
liosub <mlio64565@gmail.com>
2023-10-30 15:58:51 +0000

    add safety and security controller

--- src/main/java/auto/cc/info/controllers/SafetyAndSecurityController.java
+++ src/main/java/auto/cc/info/controllers/SafetyAndSecurityController.java
@@ -1,9 +1,12 @@
 package auto.cc.info.controllers;
 
+import auto.cc.info.commands.FeaturesCommand;
 import auto.cc.info.commands.SafetyAndSecurityCommand;
 import auto.cc.info.service.SafetyAndSecurityService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,5 +35,11 @@ public class SafetyAndSecurityController {
         else {
             return safetyAndSecurityCommandOptional.get();
         }
+    }
+
+    @QueryMapping(name = "findBySecurityId")
+    public SafetyAndSecurityCommand findBySecurityId(@Argument Long id) {
+        SafetyAndSecurityCommand safetyAndSecurityCommand = safetyAndSecurityService.findBySSId(id);
+        return safetyAndSecurityCommand;
     }
 }
