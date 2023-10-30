4bc9d671dbc174a2ab59f15d5ebfc10032bd46dc
liosub <mlio64565@gmail.com>
2023-10-30 16:07:47 +0000

    update features command

--- src/main/java/auto/cc/info/commands/FeaturesCommand.java
+++ src/main/java/auto/cc/info/commands/FeaturesCommand.java
@@ -13,7 +13,7 @@ public class FeaturesCommand {
 
     private Long id;
     private HashMap<String,String> comfort  =new HashMap<>();
-    private HashMap<String,Boolean> accessories  =new HashMap<>();
+    private HashMap<String,String> accessories  =new HashMap<>();
     private HashMap<String,String> others  =new HashMap<>();
 }
 
