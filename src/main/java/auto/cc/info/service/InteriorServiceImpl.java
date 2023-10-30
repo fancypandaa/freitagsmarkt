f80c83ccbe035c61c0056bc97e8ff748121b8822
liosub <mlio64565@gmail.com>
2023-10-30 15:56:18 +0000

    add interior service

--- src/main/java/auto/cc/info/service/InteriorServiceImpl.java
+++ src/main/java/auto/cc/info/service/InteriorServiceImpl.java
@@ -3,7 +3,8 @@ package auto.cc.info.service;
 import auto.cc.info.commands.InteriorCommand;
 import auto.cc.info.commands.InteriorEquipmentsCommand;
 import auto.cc.info.converters.InteriorCommandToInterior;
-import auto.cc.info.converters.InteriorEquipmentsCommandToInteriorEquipments;
+import auto.cc.info.converters.InEquipmentsCommandToInEquipments;
+import auto.cc.info.converters.InteriorToInteriorCommand;
 import auto.cc.info.domain.Interior;
 import auto.cc.info.domain.InteriorEquipments;
 import auto.cc.info.repository.InteriorEquipmentsRepository;
@@ -11,20 +12,22 @@ import auto.cc.info.repository.InteriorRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.util.*;
 @Slf4j
 @Service
 public class InteriorServiceImpl implements InteriorService{
     private final InteriorRepository interiorRepository;
     private final InteriorEquipmentsRepository interiorEquipmentsRepository;
-    private final InteriorEquipmentsCommandToInteriorEquipments toInteriorEquipments;
+    private final InEquipmentsCommandToInEquipments toInteriorEquipments;
     private final InteriorCommandToInterior commandToInterior;
+    private final InteriorToInteriorCommand interiorToInteriorCommand;
 
-    public InteriorServiceImpl(InteriorRepository interiorRepository, InteriorEquipmentsRepository interiorEquipmentsRepository, InteriorEquipmentsCommandToInteriorEquipments toInteriorEquipments, InteriorCommandToInterior commandToInterior) {
+    public InteriorServiceImpl(InteriorRepository interiorRepository, InteriorEquipmentsRepository interiorEquipmentsRepository, InEquipmentsCommandToInEquipments toInteriorEquipments, InteriorCommandToInterior commandToInterior, InteriorToInteriorCommand interiorToInteriorCommand) {
         this.interiorRepository = interiorRepository;
         this.interiorEquipmentsRepository = interiorEquipmentsRepository;
         this.toInteriorEquipments = toInteriorEquipments;
         this.commandToInterior = commandToInterior;
+        this.interiorToInteriorCommand = interiorToInteriorCommand;
     }
 
 
@@ -39,8 +42,20 @@ public class InteriorServiceImpl implements InteriorService{
     @Override
     @Transactional
     public InteriorCommand createInterior(InteriorCommand interiorCommand) {
+        Optional<InteriorEquipments> interiorEquipmentsOptional=
+                interiorEquipmentsRepository.findById(interiorCommand.getInteriorEquipments().getId());
+        if(!interiorEquipmentsOptional.isPresent()) return null;
         Interior interior= commandToInterior.convert(interiorCommand);
         interiorRepository.save(interior);
         return interiorCommand;
+    }
+
+    @Override
+    @Transactional
+    public InteriorCommand findInteriorById(Long id) {
+        Optional<Interior> interior =interiorRepository.findById(id);
+        log.error(interior.get().getInteriorEquipments().getId()+"xxx");
+        if(!interior.isPresent()) return null;
+        return interiorToInteriorCommand.convert(interior.get());
     }
 }
