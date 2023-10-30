cd03daf2e79f093652c18bdca84cde30ad6e89a2
liosub <mlio64565@gmail.com>
2023-10-30 16:00:36 +0000

    add features repo

--- src/main/java/auto/cc/info/repository/FeaturesRepository.java
+++ src/main/java/auto/cc/info/repository/FeaturesRepository.java
@@ -4,7 +4,9 @@ import auto.cc.info.domain.Features;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
 
+@Repository
 public interface FeaturesRepository extends JpaRepository<Features,Long> {
+    Optional<Features> findById(Long id);
 }
