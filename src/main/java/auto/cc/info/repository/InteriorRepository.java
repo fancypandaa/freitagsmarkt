e447dc50ad652ed1f45dd631934773e07911fab3
liosub <mlio64565@gmail.com>
2023-10-30 15:59:16 +0000

    add interior repo

--- src/main/java/auto/cc/info/repository/InteriorRepository.java
+++ src/main/java/auto/cc/info/repository/InteriorRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-
 public interface InteriorRepository extends JpaRepository<Interior,Long> {
 }
