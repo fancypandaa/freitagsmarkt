a26809f1fcc83320f4f32e422d7eabdef8746c48
liosub <mlio64565@gmail.com>
2023-11-02 16:22:41 +0000

    add transactional to get method

--- src/main/java/auto/cc/info/converters/CarCommandToCar.java
+++ src/main/java/auto/cc/info/converters/CarCommandToCar.java
@@ -3,6 +3,7 @@ package auto.cc.info.converters;
 import auto.cc.info.commands.CarCommand;
 import auto.cc.info.domain.Car;
 import auto.cc.info.domain.CarBrand;
+import auto.cc.info.domain.Seller;
 import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.Nullable;
@@ -11,10 +12,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CarCommandToCar implements Converter<CarCommand, Car> {
-    private final SpecsCommandToSpecs specsCommandToSpecs;
+    private final EngineCommandToEngine engineCommandToEngine;
+    private final DimAndWeightCommandToDimAndWeight commandToDimensionsAndWeight;
+    private final ExteriorCommandToExterior exteriorCommandToExterior;
+    private final  InteriorCommandToInterior commandToInterior;
+    private final FeaturesCommandToFeatures featuresCommandToFeatures;
+    private final SafetyAndSecurityCommandToSafetyAndSecurity commandToSafetyAndSecurity;
 
-    public CarCommandToCar( SpecsCommandToSpecs specsCommandToSpecs) {
-        this.specsCommandToSpecs = specsCommandToSpecs;
+    public CarCommandToCar(EngineCommandToEngine engineCommandToEngine, DimAndWeightCommandToDimAndWeight commandToDimensionsAndWeight, ExteriorCommandToExterior exteriorCommandToExterior, InteriorCommandToInterior commandToInterior, FeaturesCommandToFeatures featuresCommandToFeatures, SafetyAndSecurityCommandToSafetyAndSecurity commandToSafetyAndSecurity) {
+        this.engineCommandToEngine = engineCommandToEngine;
+        this.commandToDimensionsAndWeight = commandToDimensionsAndWeight;
+        this.exteriorCommandToExterior = exteriorCommandToExterior;
+        this.commandToInterior = commandToInterior;
+        this.featuresCommandToFeatures = featuresCommandToFeatures;
+        this.commandToSafetyAndSecurity = commandToSafetyAndSecurity;
     }
 
     @Synchronized
@@ -32,14 +43,25 @@ public class CarCommandToCar implements Converter<CarCommand, Car> {
         car.setGeneration(source.getGeneration());
         car.setMileage(source.getMileage());
         car.setPrice(source.getPrice());
+        car.setSaleStatus(source.getSaleStatus());
         if(source.getCarBrandId()!= null){
             CarBrand carBrand = new CarBrand();
             carBrand.setId(source.getCarBrandId());
             car.setCarBrand(carBrand);
             carBrand.setCars(car);
         }
-        car.setSpecs(specsCommandToSpecs.convert(source.getSpecsCommand()));
-
+        if(source.getSellerId()!= null){
+            Seller seller = new Seller();
+            seller.setId(source.getSellerId());
+            car.setSeller(seller);
+            seller.addCars(car);
+        }
+        car.setEngine(engineCommandToEngine.convert(source.getEngine()));
+        car.setDimensionsAndWeight(commandToDimensionsAndWeight.convert(source.getDimensionsAndWeight()));
+        car.setExterior(exteriorCommandToExterior.convert(source.getExterior()));
+        car.setInterior(commandToInterior.convert(source.getInterior()));
+        car.setFeatures(featuresCommandToFeatures.convert(source.getFeatures()));
+        car.setSafetyAndSecurity(commandToSafetyAndSecurity.convert(source.getSafetyAndSecurity()));
         return car;
     }
 }
--- src/main/java/auto/cc/info/converters/CarToCarCommand.java
+++ src/main/java/auto/cc/info/converters/CarToCarCommand.java
@@ -9,10 +9,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CarToCarCommand implements Converter<Car, CarCommand> {
-    private final SpecsToSpecsCommand specsToSpecsCommand;
+    private final EngineToEngineCommand engineToEngineCommand;
+    private final DimAndWeightToDimAndWeightCommand dimensionsConverter;
+    private final ExteriorToExteriorCommand exteriorCommand;
+    private final  InteriorToInteriorCommand interiorToInteriorCommand;
+    private final FeaturesToFeaturesCommand featuresCommandConverter;
+    private final SafetyAndSecurityToSafetyAndSecurityCommand securityToSafetyAndSecurityCommand;
 
-    public CarToCarCommand(SpecsToSpecsCommand specsToSpecsCommand) {
-        this.specsToSpecsCommand = specsToSpecsCommand;
+    public CarToCarCommand(EngineToEngineCommand engineToEngineCommand, DimAndWeightToDimAndWeightCommand dimensionsConverter, ExteriorToExteriorCommand exteriorCommand, InteriorToInteriorCommand interiorToInteriorCommand, FeaturesToFeaturesCommand featuresCommandConverter, SafetyAndSecurityToSafetyAndSecurityCommand securityToSafetyAndSecurityCommand) {
+        this.engineToEngineCommand = engineToEngineCommand;
+        this.dimensionsConverter = dimensionsConverter;
+        this.exteriorCommand = exteriorCommand;
+        this.interiorToInteriorCommand = interiorToInteriorCommand;
+        this.featuresCommandConverter = featuresCommandConverter;
+        this.securityToSafetyAndSecurityCommand = securityToSafetyAndSecurityCommand;
     }
 
     @Synchronized
@@ -30,8 +40,15 @@ public class CarToCarCommand implements Converter<Car, CarCommand> {
         carCommand.setGeneration(source.getGeneration());
         carCommand.setMileage(source.getMileage());
         carCommand.setPrice(source.getPrice());
-        carCommand.setSpecsCommand(specsToSpecsCommand.convert(source.getSpecs()));
+        carCommand.setSaleStatus(source.getSaleStatus());
         carCommand.setCarBrandId(source.getCarBrand().getId());
+        carCommand.setSellerId(source.getSeller().getId());
+        carCommand.setEngine(engineToEngineCommand.convert(source.getEngine()));
+        carCommand.setDimensionsAndWeight(dimensionsConverter.convert(source.getDimensionsAndWeight()));
+        carCommand.setExterior(exteriorCommand.convert(source.getExterior()));
+        carCommand.setInterior(interiorToInteriorCommand.convert(source.getInterior()));
+        carCommand.setFeatures(featuresCommandConverter.convert(source.getFeatures()));
+        carCommand.setSafetyAndSecurity(securityToSafetyAndSecurityCommand.convert(source.getSafetyAndSecurity()));
         return carCommand;
     }
 }
