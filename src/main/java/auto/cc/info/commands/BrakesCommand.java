bd8d64d91abf444d715bf169f9310a394efaff90
liosub <mlio64565@gmail.com>
2023-11-02 16:16:18 +0000

    add serializable to command

--- src/main/java/auto/cc/info/commands/BrakesCommand.java
+++ src/main/java/auto/cc/info/commands/BrakesCommand.java
@@ -5,11 +5,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.NotBlank;
 
+import java.io.Serializable;
+
 @NoArgsConstructor
 @Getter
 @Setter
-public class BrakesCommand {
-
+public class BrakesCommand implements Serializable {
+    private static final long serialVersionUID = -4439114469417994311L;
     private Long id;
     private String parkingBrake;
     private String manual;
--- src/main/java/auto/cc/info/commands/CarBrandCommand.java
+++ src/main/java/auto/cc/info/commands/CarBrandCommand.java
@@ -4,16 +4,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
-public class CarBrandCommand {
+public class CarBrandCommand implements Serializable {
+    private static final long serialVersionUID = -4439114469417994311L;
     private Long id;
     private String name;
     private String countryOfOrigin;
-    private String productionYears;
+    private Integer productionYears;
     private String series;
     private String logoUrl;
     private List<CarCommand> cars = new ArrayList<>();
