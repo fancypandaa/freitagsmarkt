e9db5b204e317c6fb7de09109a867bb4514b133f
liosub <mlio64565@gmail.com>
2023-11-02 16:17:42 +0000

    add filters queries

--- src/main/java/auto/cc/info/repository/ExteriorRepository.java
+++ src/main/java/auto/cc/info/repository/ExteriorRepository.java
@@ -1,9 +1,17 @@
 package auto.cc.info.repository;
 
+import auto.cc.info.commands.custom.IExteriorCustom;
+import auto.cc.info.commands.custom.IFuelCustom;
 import auto.cc.info.domain.Exterior;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ExteriorRepository extends JpaRepository<Exterior,Long> {
+
+    @Query(value = "SELECT chassis, COUNT(chassis) AS types from exterior group by chassis",nativeQuery = true)
+    List<IExteriorCustom> getChassisTypesByGroups();
 }
