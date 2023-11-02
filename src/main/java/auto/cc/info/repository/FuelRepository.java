9f1b5b3a863740da0e44838c7500fc983540daa4
liosub <mlio64565@gmail.com>
2023-11-02 16:17:49 +0000

    add filters queries

--- src/main/java/auto/cc/info/repository/FuelRepository.java
+++ src/main/java/auto/cc/info/repository/FuelRepository.java
@@ -1,10 +1,14 @@
 package auto.cc.info.repository;
 
 import auto.cc.info.domain.Fuel;
+import auto.cc.info.commands.custom.IFuelCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.*;
 
 @Repository
-
 public interface FuelRepository extends JpaRepository<Fuel,Long> {
+    @Query(value = "SELECT fuel_type AS fuelType, COUNT(fuel_type) AS types from fuel group by fuel_type",nativeQuery = true)
+    List<IFuelCustom> getFuelTypesByGroups();
 }
