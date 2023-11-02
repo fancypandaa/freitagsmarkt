52738054fad129f64051483ff2e7a08993505de8
liosub <mlio64565@gmail.com>
2023-11-02 16:10:02 +0000

    remove specs

--- src/main/java/auto/cc/info/service/SpecsService.java
+++
@@ -1,8 +0,0 @@
-package auto.cc.info.service;
-
-import auto.cc.info.commands.SpecsCommand;
-
-public interface SpecsService {
-    SpecsCommand createNewSpecs(SpecsCommand specsCommand);
-    SpecsCommand findSpecsById(Long id);
-}
