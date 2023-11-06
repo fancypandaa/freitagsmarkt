f38ca286093fd17c391bff9972fe9d3f3caedee5
liosub <mlio64565@gmail.com>
2023-11-06 21:22:50 +0000

    create jwt token creator

---
+++ src/main/java/auto/cc/info/security/JwtGenerator.java
@@ -0,0 +1,29 @@
+package auto.cc.info.security;
+
+import auto.cc.info.commands.UserCommand;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+@Component
+public class JwtGenerator {
+    public String generate(UserCommand userCommand){
+        Date date = new Date();
+        date.setHours(date.getHours()+2);
+        String secretKey= "Freedom is slavery";
+        AESEncryptionDecryption aesEncryptionDecryption = new AESEncryptionDecryption();
+        String userName = aesEncryptionDecryption.encrypt(userCommand.getUserName(), secretKey);
+        String password = aesEncryptionDecryption.encrypt(userCommand.getPassword(), secretKey);
+
+        Claims claims = Jwts.claims()
+                .setSubject(userName);
+        claims.put("password",password);
+        claims.put("role",userCommand.getRole());
+        return Jwts.builder().setClaims(claims)
+                .setExpiration(date)
+                .signWith(SignatureAlgorithm.HS512,"$2a$10$EWk4NyP6rnCeeuMKNogTgO0lU43BQT/XjS.dLVcMwO0JBfQ75zqiW")
+                .compact();
+    }
+
+}
