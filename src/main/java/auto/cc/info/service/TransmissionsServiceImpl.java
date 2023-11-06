ceb5515673ff3e5d7bd0baddb2e60b2256ddd068
liosub <mlio64565@gmail.com>
2023-11-06 21:26:19 +0000

    fixes

--- src/main/java/auto/cc/info/service/TransmissionsServiceImpl.java
+++ src/main/java/auto/cc/info/service/TransmissionsServiceImpl.java
@@ -1,14 +1,9 @@
 package auto.cc.info.service;
 
-import auto.cc.info.commands.SuspensionsCommand;
 import auto.cc.info.commands.TransmissionCommand;
-import auto.cc.info.converters.SuspensionsCommandToSuspensions;
-import auto.cc.info.converters.SuspensionsToSuspensionsCommand;
 import auto.cc.info.converters.TransmissionCommandToTransmission;
 import auto.cc.info.converters.TransmissionToTransmissionCommand;
-import auto.cc.info.domain.Suspensions;
 import auto.cc.info.domain.Transmission;
-import auto.cc.info.repository.SuspensionsRepository;
 import auto.cc.info.repository.TransmissionRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
