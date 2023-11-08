be69e4b1dd3257f0c657007b8284bee2e182074f
liosub <mlio64565@gmail.com>
2023-11-08 20:15:29 +0000

    set api(s) roles for car

--- src/main/java/auto/cc/info/controllers/CarController.java
+++ src/main/java/auto/cc/info/controllers/CarController.java
@@ -4,6 +4,7 @@ import auto.cc.info.commands.BrakesCommand;
 import auto.cc.info.commands.CarBrandCommand;
 import auto.cc.info.commands.CarCommand;
 import auto.cc.info.domain.Car;
+import auto.cc.info.domain.user.Constants;
 import auto.cc.info.service.CarService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,11 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/car")
+@RequestMapping("/api/car")
 @Slf4j
 public class CarController {
     private CarService carService;
@@ -25,6 +27,7 @@ public class CarController {
         this.carService = carService;
     }
     @RequestMapping(value = "",method = RequestMethod.POST,produces = "application/json")
+    @RolesAllowed(Constants.SELLER)
     public CarCommand addNewCar(@RequestBody CarCommand carCommand){
         Optional<CarCommand> carCommandOptional = Optional.ofNullable(carService.addNewCar(carCommand));
         if(!carCommandOptional.isPresent()){
@@ -36,20 +39,24 @@ public class CarController {
         }
     }
     @RequestMapping(value = "/{carId}",method = RequestMethod.PATCH,produces = "application/json")
+    @RolesAllowed(Constants.SELLER)
     public CarCommand updateCar(@PathVariable Long carId, @RequestBody CarCommand carCommand){
         CarCommand carCommand1 = carService.updateCarById(carId,carCommand);
         return carCommand1;
     }
     @RequestMapping(value = "{carId}",method = RequestMethod.DELETE,produces = "application/json")
+    @RolesAllowed(Constants.SELLER)
     public void deleteCarById(@PathVariable Long carId){
         carService.removeCarById(carId);
     }
     @QueryMapping("listAllCars")
+    @RolesAllowed({Constants.USER,Constants.SELLER})
     public Page<CarCommand> listAllCars(@Argument int page, @Argument int size){
         Page<CarCommand> carCommands = carService.listCars(page,size);
         return carCommands;
     }
     @QueryMapping(name = "findCarById")
+    @RolesAllowed({Constants.USER,Constants.SELLER})
     public CarCommand findCarById(@Argument Long id) {
         CarCommand car = carService.findCarById(id);
         return car;
