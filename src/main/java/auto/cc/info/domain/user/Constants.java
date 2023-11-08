6dfa836ac1e0a6d096769e117b3f88ff947f3ccb
liosub <mlio64565@gmail.com>
2023-11-08 20:17:25 +0000

    create constant class for static values

---
+++ src/main/java/auto/cc/info/domain/user/Constants.java
@@ -0,0 +1,9 @@
+package auto.cc.info.domain.user;
+
+public class Constants {
+    public static final String GUEST = "ROLE_GUEST";
+    public static final String USER = "ROLE_USER";
+    public static final String SELLER = "ROLE_SELLER";
+    public static final String SELLER_GUEST="ROLE_SELLER_GUEST";
+
+}
