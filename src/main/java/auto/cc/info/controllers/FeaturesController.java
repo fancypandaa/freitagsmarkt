7aa3ca1e982b59b8fcd0becfecfbe81bb4453e54
liosub <mlio64565@gmail.com>
2023-11-08 20:16:04 +0000

    set api(s) roles for features

--- src/main/java/auto/cc/info/controllers/FeaturesController.java
+++ src/main/java/auto/cc/info/controllers/FeaturesController.java
@@ -2,6 +2,7 @@ package auto.cc.info.controllers;
 
 import auto.cc.info.commands.DimensionsAndWeightCommand;
 import auto.cc.info.commands.FeaturesCommand;
+import auto.cc.info.domain.user.Constants;
 import auto.cc.info.service.FeaturesService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,11 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/features")
+@RequestMapping("/api/features")
 @Slf4j
 public class FeaturesController {
     private FeaturesService featuresService;
@@ -21,6 +23,7 @@ public class FeaturesController {
         this.featuresService = featuresService;
     }
     @RequestMapping(value = "",method = RequestMethod.POST,produces = "application/json")
+    @RolesAllowed(Constants.SELLER)
     public FeaturesCommand addNewCarFeatures(@RequestBody FeaturesCommand featuresCommand){
         Optional<FeaturesCommand> featuresCommandOptional = Optional.ofNullable(featuresService.addNewCarFeatures(featuresCommand));
         if(!featuresCommandOptional.isPresent()){
@@ -32,6 +35,7 @@ public class FeaturesController {
         }
     }
     @QueryMapping(name = "findFeaturesById")
+    @RolesAllowed({Constants.USER,Constants.SELLER})
     public FeaturesCommand findFeaturesById(@Argument Long id) {
         FeaturesCommand featuresCommand = featuresService.findFeaturesById(id);
         return featuresCommand;
