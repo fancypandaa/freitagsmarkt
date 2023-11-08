9026a5999ae0793db4bf3158366bc362f84fafa0
liosub <mlio64565@gmail.com>
2023-11-08 20:15:50 +0000

    set api(s) roles for engine

--- src/main/java/auto/cc/info/controllers/EngineController.java
+++ src/main/java/auto/cc/info/controllers/EngineController.java
@@ -2,6 +2,7 @@ package auto.cc.info.controllers;
 
 import auto.cc.info.commands.AdsCommand;
 import auto.cc.info.commands.EngineCommand;
+import auto.cc.info.domain.user.Constants;
 import auto.cc.info.service.EngineService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +11,11 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/engine")
+@RequestMapping("/api/engine")
 @Slf4j
 public class EngineController {
     private EngineService engineService;
@@ -23,6 +25,7 @@ public class EngineController {
     }
 
     @RequestMapping(value = "",method = RequestMethod.POST,produces = "application/json")
+    @RolesAllowed(Constants.SELLER)
     public EngineCommand addNewEngine(@RequestBody EngineCommand engineCommand){
         Optional<EngineCommand> engineCommandOptional = Optional.ofNullable(engineService.addNewEngineDetails(engineCommand));
         if(!engineCommandOptional.isPresent()){
@@ -34,10 +37,12 @@ public class EngineController {
         }
     }
     @RequestMapping(value = "{engineId}",method = RequestMethod.DELETE,produces = "application/json")
+    @RolesAllowed(Constants.SELLER)
     public void deleteEngineById(@PathVariable Long engineId){
         engineService.removeEngineById(engineId);
     }
     @QueryMapping("listAllEngine")
+    @RolesAllowed({Constants.USER,Constants.SELLER})
     public Page<EngineCommand> listAllEngine(@Argument int page, @Argument int size){
         Page<EngineCommand> engineCommandPage = engineService.listEngines(page,size);
         return engineCommandPage;
