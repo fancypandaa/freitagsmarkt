59a310edd862939405e2a4d0e07b4c4499810046
liosub <mlio64565@gmail.com>
2023-11-08 20:16:09 +0000

    set api(s) roles for fuel

--- src/main/java/auto/cc/info/controllers/FuelController.java
+++ src/main/java/auto/cc/info/controllers/FuelController.java
@@ -2,6 +2,7 @@ package auto.cc.info.controllers;
 
 import auto.cc.info.commands.FuelCommand;
 import auto.cc.info.commands.custom.IFuelCustom;
+import auto.cc.info.domain.user.Constants;
 import auto.cc.info.service.FuelService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.*;
 
 @RestController
-@RequestMapping("/fuel")
+@RequestMapping("/api/fuel")
 @Slf4j
 public class FuelController {
     private FuelService fuelService;
@@ -23,6 +25,7 @@ public class FuelController {
         this.fuelService = fuelService;
     }
     @RequestMapping(value = "",method = RequestMethod.POST,produces = "application/json")
+    @RolesAllowed(Constants.SELLER)
     public FuelCommand addFuelInfo(@RequestBody FuelCommand fuelCommand){
         Optional<FuelCommand> fuelCommandOptional = Optional.ofNullable(fuelService.addFuelInfo(fuelCommand));
         if(!fuelCommandOptional.isPresent()){
@@ -34,6 +37,7 @@ public class FuelController {
         }
     }
     @QueryMapping(name = "getFuelTypesByGroups")
+    @RolesAllowed({Constants.USER,Constants.SELLER})
     public List<IFuelCustom> getFuelTypesByGroups(){
         List<IFuelCustom> fuelCommands = fuelService.getFuelTypesByGroups();
         return fuelCommands;
