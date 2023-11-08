1668a7d42cec3c00246c7beedce889e9fd3b9040
liosub <mlio64565@gmail.com>
2023-11-08 20:19:03 +0000

    add auto seller guest token generator

--- src/main/java/auto/cc/info/config/SwaggerConfig.java
+++ src/main/java/auto/cc/info/config/SwaggerConfig.java
@@ -1,19 +1,22 @@
 package auto.cc.info.config;
+import auto.cc.info.commands.UserCommand;
+import auto.cc.info.domain.user.Constants;
+import auto.cc.info.security.JwtGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
+    @Autowired
+    private JwtGenerator jwtGenerator;
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
@@ -30,5 +33,14 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    @Bean
+    void generateSellerGuestToken(){
+        UserCommand userCommand = new UserCommand();
+        userCommand.setUserName("TIC_TOC");
+        userCommand.setPassword("_TOC_TIC");
+        userCommand.setRole("SELLER_GUEST");
+        String token = jwtGenerator.generate(userCommand);
+        System.out.println("TOKEN_SE: "+token+" ROLE : " + userCommand.getRole());
     }
 }
