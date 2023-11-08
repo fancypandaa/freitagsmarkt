827b7a073f17799600c4d3469b64a2b4ce65878d
liosub <mlio64565@gmail.com>
2023-11-08 20:22:30 +0000

    add api(s) roles for transmissions

--- src/main/java/auto/cc/info/controllers/TransmissionsController.java
+++ src/main/java/auto/cc/info/controllers/TransmissionsController.java
@@ -1,8 +1,7 @@
 package auto.cc.info.controllers;
 
-import auto.cc.info.commands.SuspensionsCommand;
 import auto.cc.info.commands.TransmissionCommand;
-import auto.cc.info.service.SuspensionsService;
+import auto.cc.info.domain.user.Constants;
 import auto.cc.info.service.TransmissionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/transmissions")
+@RequestMapping("/api/transmissions")
 @Slf4j
 public class TransmissionsController {
     private TransmissionService transmissionService;
@@ -25,6 +25,7 @@ public class TransmissionsController {
     }
 
     @RequestMapping(value = "",method = RequestMethod.POST,produces = "application/json")
+    @RolesAllowed(Constants.SELLER)
     public TransmissionCommand addTransmissionInfo(@RequestBody TransmissionCommand transmissionCommand){
         Optional<TransmissionCommand> transmissionCommand1 = Optional.ofNullable(transmissionService.createNewTransmissionsItems(transmissionCommand));
         if(!transmissionCommand1.isPresent()){
