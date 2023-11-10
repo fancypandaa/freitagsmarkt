3502d7803c20f6b78233fe0bdb8eca2ae34e8a3e
liosub <mlio64565@gmail.com>
2023-11-10 19:13:39 +0000

    add class diagrams

--- src/main/java/auto/cc/info/security/JwtSuccessHandler.java
+++ src/main/java/auto/cc/info/security/JwtSuccessHandler.java
@@ -3,7 +3,6 @@ package auto.cc.info.security;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
