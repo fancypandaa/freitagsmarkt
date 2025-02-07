7280c93216719e5e786c0563478ed07afcfb2aa3
fancypanda <nlio7585@proton.me>
2025-02-07 17:10:40 +0000

    #RR [12] update features full cycle

--- src/main/java/auto/freitagsmarkt/service/FeaturesServiceImpl.java
+++ src/main/java/auto/freitagsmarkt/service/FeaturesServiceImpl.java
@@ -1,17 +1,37 @@
-package auto.cc.info.service;
+package auto.freitagsmarkt.service;
 
-import auto.cc.info.dto.otherComponents.FeaturesCommand;
+
+import auto.freitagsmarkt.dto.car.otherComponents.FeaturesDTO;
+import auto.freitagsmarkt.mapper.otherComponents.FeaturesMapper;
+import auto.freitagsmarkt.repository.FeaturesRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class FeaturesServiceImpl implements FeaturesService{
-    @Override
-    public FeaturesCommand addNewCarFeatures(FeaturesCommand featuresCommand) {
-        return null;
+
+    private FeaturesMapper featuresMapper;
+    private FeaturesRepository featuresRepository;
+
+    public FeaturesServiceImpl(FeaturesMapper featuresMapper, FeaturesRepository featuresRepository) {
+        this.featuresMapper = featuresMapper;
+        this.featuresRepository = featuresRepository;
     }
 
     @Override
-    public FeaturesCommand findFeaturesById(Long id) {
-        return null;
+    public FeaturesDTO addNewCarFeatures(FeaturesDTO featuresDTO) {
+        return Optional.of(featuresDTO)
+                .map(featuresMapper::toFeatures)
+                .map(featuresRepository::save)
+                .map(featuresMapper::toFeaturesDTO)
+                .orElseThrow(() -> new RuntimeException("Cannot create New car feature"));
+    }
+
+    @Override
+    public FeaturesDTO findFeaturesById(Long featuresId) {
+        return featuresRepository.findById(featuresId)
+                .map(featuresMapper::toFeaturesDTO)
+                .orElseThrow(()-> new RuntimeException("Feature not found"));
     }
 }
